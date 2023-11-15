042c58a86523fbb05c1457fd908b781a18dfc041
RazielBlaster <jhojanblackout@gmail.com>
2023-11-15 16:40:09 -0500

    add htmlfor

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,28 +11,28 @@ const Contact = () => {
 
 
   return (
-    <Form className='contact' netlify>
+    <Form className='contact' netlify netlify-honeypot="bot-field" >
 
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label className='contact__label'>Nombre</Form.Label>
-        <Form.Control type="text"  required/>
+        <Form.Label className='contact__label' htmlFor='name' >Nombre</Form.Label>
+        <Form.Control type="text" name='name' id='name' required/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label className='contact__label'>Correo electrónico</Form.Label>
-        <Form.Control type="email"  required/>
+        <Form.Label className='contact__label' htmlFor='email'>Correo electrónico</Form.Label>
+        <Form.Control type="email" name='email' id='email'  required/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label className='contact__label'>Celular</Form.Label>
-        <Form.Control type="number" placeholder='¿Quieres que te contactemos?' onkeydown="return false" onwheel="return false" />
+        <Form.Label className='contact__label' htmlFor='phone'>Celular</Form.Label>
+        <Form.Control type="number" name='phone' id='phone' placeholder='¿Quieres que te contactemos?' onkeydown="return false" onwheel="return false" />
       </Form.Group>
 
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label className='contact__label'>Tu mensaje</Form.Label>
-        <Form.Control as="textarea" rows={3} placeholder='Puedes incluir ejemplos para darte un precio aproximado' required />
+        <Form.Label className='contact__label' htmlFor='message'>Tu mensaje</Form.Label>
+        <Form.Control as="textarea" name='message' id='message' rows={3} placeholder='Puedes incluir ejemplos para darte un precio aproximado' required />
       </Form.Group>
 
 
