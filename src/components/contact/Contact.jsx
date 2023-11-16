36c96fc90340422c931e9de25cfcdf6e9b08d685
RazielBlaster <jhojanblackout@gmail.com>
2023-11-16 09:27:38 -0500

    Try form easy

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './contact.css'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+
 
 
 
@@ -10,43 +9,24 @@ const Contact = () => {
 
 
 
+  //netlify-honeypot="bot-field"
+
+
+
   return (
-    <Form className='contact' netlify netlify-honeypot="bot-field" >
+    
+    <form name="contact" netlify >
+
+      <label>
+        Name: 
+        <input type="text" name="name" id="name" />
+      </label>
 
 
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Nombre
-        <Form.Control type="text" name='name' id='name' required/>
-        </Form.Label>
-      </Form.Group>
+      <button type="submit"> Enviar </button>
 
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label >Correo electrónico
+    </form>
 
-        <Form.Control type="email" name='email' id='email'  required/>
-        </Form.Label>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label >Celular
-
-        <Form.Control type="number" name='phone' id='phone' placeholder='¿Quieres que te contactemos?' onkeydown="return false" onwheel="return false" />
-        </Form.Label>
-      </Form.Group>
-
-
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label >Tu mensaje
-
-        <Form.Control as="textarea" name='message' id='message' rows={3} placeholder='Puedes incluir ejemplos para darte un precio aproximado' required />
-        </Form.Label>
-      </Form.Group>
-
-
-      <Button variant="warning" type="submit">
-        Enviar
-      </Button>
-    </Form>
   )
 }
 
