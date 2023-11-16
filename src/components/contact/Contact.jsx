42d6d1bd01a9c51f61421bdbb72aacfb1f2d9fcc
RazielBlaster <jhojanblackout@gmail.com>
2023-11-16 13:16:41 -0500

    Fixed form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,21 +15,46 @@ const Contact = () => {
 
   return (
 
-    <form name="contact" method="post">
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label>Your Name: <input type="text" name="name"/></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email"/></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+    <form name="contact" method="post" className='contact'>
+      <input type="hidden" name="form-name" value="contact" />
+
+      <div className='contact__element'>
+        <label className='contact__label'>
+          Tu nombre:
+          <input className='contact__input' type="text" name="name" required />
+        </label>
+      </div>
+
+      <div className='contact__element'>
+        <label className='contact__label'>
+          Email:
+          <input className='contact__input' type="email" name="email" required />
+        </label>
+      </div>
+
+      <div className='contact__element'>
+        <label className='contact__label'>
+          Número de contacto:
+          <input className='contact__input' type="number" name="phone" />
+        </label>
+      </div>
+
+      <div className='contact__element'>
+        <label className='contact__label'>
+          Mensaje:
+          <textarea className='contact__input contact__textarea'
+            name="message"
+            required
+            placeholder='Puedes incluir ejemplos para brindarte mejor información'>
+          </textarea>
+        </label>
+      </div>
+
+      <div className='contact__element'>
+        <button className='contact__send' type="submit">Enviar</button>
+      </div>
+
+    </form>
   )
 }
 
