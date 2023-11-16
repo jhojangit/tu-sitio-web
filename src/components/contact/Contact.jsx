5f61c0a5744e9e6c565c007ff261079c1b16dacf
RazielBlaster <jhojanblackout@gmail.com>
2023-11-16 09:42:49 -0500

    Try form example netlify

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,17 +15,21 @@ const Contact = () => {
 
   return (
 
-    <form name="contact" netlify>
-      <p>
-        <label>Name <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Email <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <form name="contact" method="post">
+    <input type="hidden" name="form-name" value="contact" />
+    <p>
+      <label>Your Name: <input type="text" name="name"/></label>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email"/></label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
   )
 }
 
